5780f31b615d746cab29a0217da891714392174a
Miqueas Aguilar <aguilarmiqueas@gmail.com>
2023-06-03 07:05:36 -0300

    untracked

---
+++ components/navbar/navbar.js
@@ -0,0 +1,12 @@
+import styles from "./navbar.module.scss"
+import Link from "next/link"
+export const Navbar = () => {
+    return (<nav className={styles.navbar}>
+        <Link href="/">SIMULTÁNEO</Link>
+        <ul>
+            <Link href="/bio">Bio</Link>
+            <Link href="/previous">Previous Works</Link>
+            <Link href="/contact">Contact</Link>
+        </ul>
+    </nav>)
+}
